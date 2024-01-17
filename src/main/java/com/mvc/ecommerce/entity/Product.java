0e2194011626b71a98bfa136596cbb8b4cb5cb95
GauravPandey3112 <gauravpandey31@yahoo.com>
2024-01-17 23:31:11 +0530

    Added code for product entity and other folders

---
+++ src/main/java/com/mvc/ecommerce/entity/Product.java
@@ -0,0 +1,62 @@
+package com.mvc.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="product_details")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private long productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_price")
+    private long productPrice;
+    @Column(name = "quantity")
+    private long productQty;
+
+    public Product() {
+    }
+
+    public Product(long productId, String productName, long productPrice, long productQty) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQty = productQty;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(long productPrice) {
+        this.productPrice = productPrice;
+    }
+    public long getProductQty() {
+        return productQty;
+    }
+
+    public void setProductQty(long productQty) {
+        this.productQty = productQty;
+    }
+
+}
